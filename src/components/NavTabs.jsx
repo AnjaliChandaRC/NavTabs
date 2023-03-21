6aff962fecbeb431a0e91170da17bccc99483e9d
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-03-21 15:44:13 +0530

    first commit

---
+++ src/components/NavTabs.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const NavTabs = () => {
+  const tabsData = [
+    {
+      heading: "One",
+      para: "One",
+    },
+    {
+      heading: "Two",
+      para: "Two",
+    },
+    {
+      heading: "Three",
+      para: "Three",
+    },
+    {
+      heading: "Four",
+      para: "Four",
+    },
+    {
+      heading: "Five",
+      para: "Five",
+    },
+  ];
+  const [value, setValue] = useState(tabsData[0]);
+    console.log(value, "value");
+    
+
+
+
+  return (
+    <>
+      <div className="text-center mt-5 pt-5">
+        {tabsData.map((val, index) => {
+          return (
+              <button
+                key={index}
+                className={`${
+                  val == value ? "bg-success text-white" : "bg-info text-success"
+                } ms-3 fs-3 fw-bold border-0 py-2 px-5`}
+                onClick={() => setValue(val)}
+              >
+                {val.heading}
+              </button>
+          );
+        })}
+        <p className="pt-5 fs-1 fw-semibold text-primary">{value.para}</p>
+      </div>
+    </>
+  );
+};
+
+export default NavTabs;
