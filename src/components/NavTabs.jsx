13c9f328590e526fcba564220a8c7f844f110822
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-03-24 10:21:10 +0530

    done navtabs

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -24,25 +24,24 @@ const NavTabs = () => {
     },
   ];
   const [value, setValue] = useState(tabsData[0]);
-    console.log(value, "value");
-    
-
-
+  console.log(value, "value");
 
   return (
     <>
       <div className="text-center mt-5 pt-5">
         {tabsData.map((val, index) => {
           return (
-              <button
-                key={index}
-                className={`${
-                  val == value ? "bg-success text-white" : "bg-info text-success"
-                } ms-3 fs-3 fw-bold border-0 py-2 px-5`}
-                onClick={() => setValue(val)}
-              >
-                {val.heading}
-              </button>
+            <button
+              key={index}
+              className={`${
+                val.para == value.para
+                  ? "bg-success text-white"
+                  : "bg-info text-success"
+              } ms-3 fs-3 fw-bold border-0 py-2 px-5`}
+              onClick={() => setValue(val)}
+            >
+              {val.heading}
+            </button>
           );
         })}
         <p className="pt-5 fs-1 fw-semibold text-primary">{value.para}</p>
